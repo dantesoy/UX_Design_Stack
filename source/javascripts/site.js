2940625dc6e8d464a368be6bee318b1fb2e978be
Walter Reyes <wal.reyes.11@gmail.com>
2018-09-04 16:37:56 -0500

    Display single user categories with spider web chart

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -137,6 +137,144 @@ class CsvDataToPersonsHashFormatter {
 
 // -- End CsvToPersonHash --
 
+// -- Begin CategoryHtmlContent --
+class CategoryHtmlContent {
+  static categoryRow(categoryName, subCategories) {
+    let subCategoriesHtml = "";
+
+    const subCategoriesKeys = Object.keys(subCategories);
+    subCategoriesKeys.forEach(subCategoryKey => {
+      subCategoriesHtml += this.subCategory(
+        subCategoryKey,
+        subCategories[subCategoryKey]
+      );
+    });
+
+    return `
+      <div class="separation-top row align-flex vertical" id="category-${categoryName}">
+        <p class="info-title col-2">${categoryName}</p>
+        <div class="row info text-center col-10" id="subcategories-container">
+          ${subCategoriesHtml}
+        </div>
+      </div>
+    `;
+  }
+
+  static subCategory(subCategoryName, size) {
+    const SIZES_CLASSES = {
+      3: "",
+      2: "porcentage-mid",
+      1: "porcentage-small",
+      0: "porcentage-tiny"
+    };
+
+    return `
+      <div class=col-2>
+        <div class="info-single align-flex is-column center">
+          <div class="circle-container align-flex vertical">
+            <div class="porcentage ${SIZES_CLASSES[size]}"></div>
+          </div>
+          <p>${subCategoryName}</p>
+        </div>
+      </div>
+    `;
+  }
+}
+
+// -- End CategoryHtmlConent --
+
+// -- Begin SpiderWebChart --
+class SpiderWebChart {
+  static config(subCategories) {
+    const subCategoriesKeys = Object.keys(subCategories);
+    const subCategoriesValues = subCategoriesKeys.map(subCategoryKey => {
+      return subCategories[subCategoryKey];
+    });
+    return {
+      chart: {
+        polar: true,
+        type: "area",
+        backgroundColor: "rgba(255, 255, 255, 0.0)"
+      },
+
+      title: {
+        text: "",
+        x: -80
+      },
+
+      pane: {
+        size: "80%"
+      },
+
+      xAxis: {
+        categories: subCategoriesKeys,
+        tickmarkPlacement: "on",
+        lineWidth: 0
+      },
+
+      yAxis: {
+        gridLineInterpolation: "polygon",
+        lineWidth: 0,
+        min: 0,
+        labels: {
+          enabled: false //default is true
+        }
+      },
+
+      tooltip: {
+        shared: true,
+        pointFormat:
+          '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
+      },
+
+      legend: {
+        enabled: false
+      },
+      labels: {
+        enabled: false
+      },
+      series: [
+        {
+          name: "Designer",
+          data: subCategoriesValues,
+          pointPlacement: "on",
+          color: "#7bd500"
+        }
+      ],
+      credits: {
+        enabled: false
+      },
+      navigation: {
+        buttonOptions: {
+          enabled: false
+        }
+      }
+    };
+  }
+
+  static subCategory(subCategoryName, size) {
+    const SIZES_CLASSES = {
+      3: "",
+      2: "porcentage-mid",
+      1: "porcentage-small",
+      0: "porcentage-tiny"
+    };
+
+    return `
+      <div class=col-2>
+        <div class="info-single align-flex is-column center">
+          <div class="circle-container align-flex vertical">
+            <div class="porcentage ${SIZES_CLASSES[size]}"></div>
+          </div>
+          <p>${subCategoryName}</p>
+        </div>
+      </div>
+    `;
+  }
+}
+
+// -- End CategoryHtmlConent --
+
 // -- Begin StackRenderer --
 
 class StackRenderer {
@@ -145,32 +283,43 @@ class StackRenderer {
   }
 
   static renderCategory(categoryName, subCategoriesValues) {
-    //$("span#username").html(username);
+    $("div#categories-container").append(
+      CategoryHtmlContent.categoryRow(categoryName, subCategoriesValues)
+    );
   }
 }
 // -- End StackRenderer --
+
+// -- Begin Main --
 
 Parser.parse(stackCsvPath).then(results => {
   const csvData = results.data;
   const personsCategoriesHash = CsvDataToPersonsHashFormatter.format(csvData);
 
   // For a single person
-  const singleUser = personsCategoriesHash["ramiro@icalialabs.com"];
-  StackRenderer.renderUsername("ramiro@icalialabs.com");
+  const singleUser = personsCategoriesHash["dante@icalialabs.com"];
+  StackRenderer.renderUsername("dante@icalialabs.com");
 
   const categoriesKeys = Object.keys(singleUser.categories);
+
   categoriesKeys.forEach(categoryKey => {
     StackRenderer.renderCategory(
       categoryKey,
       singleUser.categories[categoryKey]
     );
   });
+
+  $("#chart-container").highcharts(
+    SpiderWebChart.config(singleUser.categoriesAverages)
+  );
 });
+
+// -- End Main --
 
 // From the single persons gather the average for the whole design team.
 
 // For a Single Person
-// * [] Display a single person
+// * [x] Display a single person
 // * [] Add a select to display the persons in the team
 // * [] Depending on the selected person display their stack.
 // * [] Display a polar spider chart with the data.
